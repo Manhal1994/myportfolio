caccf0c81cc7ab3db0b110d092471f1c777cc5b8
Manhal khwashki <manhal.akram.khwashki@gmail.com>
2024-02-19 03:18:13 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import Main from "./componets/Main";
+import Header from "./componets/Header";
+import "./App.css";
+import Skills from "./componets/Skills";
+import Footer from "./componets/Footer";
+import MyProjects from "./componets/MyProjects";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div style={{ height: 40 + "px" }}></div>
+      <Main />
+      <section class="skills" id="skills">
+        <div class="skills-container">
+          <h2 className="skills-title">Skills</h2>
+          <p className="desc">You Can See My Skills Here Yasta</p>
+          <Skills
+            items={[
+              {
+                name: "BackEnd Development",
+                rate: "70",
+              },
+              {
+                name: "FrontEnd Development",
+                rate: "80",
+              },
+              {
+                name: "Data Science",
+                rate: "50",
+              },
+              {
+                name: "Mobile Development",
+                rate: "75",
+              },
+            ]}
+          />
+        </div>
+      </section>
+      <MyProjects />
+      <Footer />
+    </>
   );
 }
 
